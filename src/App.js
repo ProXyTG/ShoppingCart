62d4382d671f56c90cfbdb759cf885aaf0952b3d
ProXyTG <proxytg1@gmail.com>
2019-05-28 18:32:33 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import Products from './components/Products';
+import Basket from './components/Basket';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [], filteredProducts: [], cartItems: [] };
+    this.handleAddToCart = this.handleAddToCart.bind(this);
+    this.handleRemoveFromCart = this.handleRemoveFromCart.bind(this);
+  }
+
+  componentWillMount() {
+    fetch("http://localhost:8000/products").then(res => res.json())
+    .then(data => this.setState({
+      products: data,
+      filteredProducts: data
+    }));
+    if (localStorage.getItem('cartItems')) {
+      this.setState({ cartItems: JSON.parse(localStorage.getItem('cartItems')) });
+    }
+  }
+
+  handleAddToCart(e, product) {
+    this.setState(state=>{
+      const cartItems = state.cartItems;
+      let productAllreadyInCart= false;
+      cartItems.forEach(item => {
+        if(item.id===product.id){
+          productAllreadyInCart = true;
+          item.count++;
+        }
+      });
+      if(!productAllreadyInCart){
+        cartItems.push({...product, count:1});
+      }
+      localStorage.setItem("cartItems", JSON.stringify(cartItems));
+      return cartItems;
+    })
+  }
+
+  handleRemoveFromCart(e, item) {
+    this.setState(state=>{
+      const cartItems = state.cartItems.filter(elm => elm.id !== item.id);
+      localStorage.setItem('cartItem', cartItems);
+      return {cartItems};
+    });
+  }
+
+  render() {
+     return (
+       <div className="container">
+         <h1>Shopping-Cart</h1>
+         <hr />
+         <div className="row">
+           <div className="col-md-9">
+             <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart} />
+           </div>
+           <div className="col-md-3">
+             <Basket cartItems={this.state.cartItems} handleRemoveFromCart={this.handleRemoveFromCart} />
+           </div>
+
+         </div>
+
+       </div>
+     );
+   }
 }
 
 export default App;
