3c0361d0c2f7f18ca033ca01bb7ea939fde1d609
ProXyTG <proxytg1@gmail.com>
2019-06-06 15:54:15 +0200

    gotovo

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -56,18 +56,33 @@ export default class Basket extends Component {
                 </b>
 
                   <div>
-                  <button style={{ float: 'right' }} className="btn btn-danger btn-xs"
-                          onClick={this.props.handleOpenModal}>Trigger Modal</button>
+                  <button className="btn btn-primary"
+                          onClick={this.props.handleOpenModal}>Checkout</button>
                   <Modal
                   isOpen={this.props.showModal}
                     contentLabel="onRequestClose Example"
                     onRequestClose={this.props.handleCloseModal}
                     className="Modal"
-                    overlayClassName="Overlay"
-                    >
-                    <p>Modal text!</p>
-                    <button style={{ float: 'right' }} className="btn btn-danger btn-xs"
-                    onClick={this.props.handleCloseModal}>Close Modal</button>
+                    overlayClassName="Overlay">
+
+                  <form role="form">
+                    <div className="form-group">
+                      <label htmlFor="Email address">Email address</label>
+                      <input type="email" className="form-control" id="Email address" placeholder="Enter email" required={true} />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="Address">Address</label>
+                      <input type="text" className="form-control" id="Address" placeholder="Address" required={true} />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="Credit card">Credit Card</label>
+                      <input type="text" maxLength="16" className="form-control" id="Credit card detials" placeholder="Credit Card Detials" required={true}/>
+                    </div>
+                    <button type="submit" className="btn btn-default">Submit</button>
+                  </form>
+                  <button type="submit"
+                  className="btn btn-primary"
+                  onClick={this.props.handleCloseModal}>Close</button>
                     </Modal>
                     </div>
 
